c33f92e47a907bf9b2a15d44ed0cb3606177ac23
Mai Yang <161374680+mcyang12@users.noreply.github.com>
2024-04-26 16:02:41 -0500

    Delete src/mediaItems.js

--- src/mediaItems.js
+++
@@ -1,8 +0,0 @@
-const mediaItems = [
-    { id: 1, type: 'VHS', title: 'Back to the Future' },
-    { id: 2, type: 'DVD', title: 'The Matrix' },
-    { id: 3, type: 'CD', title: 'Thriller - Michael Jackson' }
-  ];
-  
-  export default mediaItems;
-  
