58829b2669b0c034d09d8f868701563071839f43
Mai Yang <161374680+mcyang12@users.noreply.github.com>
2024-04-26 16:03:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,53 @@
-import React, { useState, useReducer } from 'react';
-import { Router, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import MediaList from './MediaList';
-import mediaItems from './mediaItems';
-import Home from './Home';
-import MediaDetail from './MediaDetail';
-import Message from './Message';
-
-//YouTube channel assist: Web Dev Simplified
+import MediaItem from './MediaItem';
 
 function App() {
+  const [mediaItems, setMediaItems] = useState([
+    { id: 1, type: 'VHS', title: 'Back to the Future', isFavorite: false },
+    { id: 2, type: 'DVD', title: 'The Matrix', isFavorite: false },
+    { id: 3, type: 'CD', title: 'Thriller - Michael Jackson', isFavorite: false }
+  ]);
+
   const [cart, setCart] = useState([]);
-  const [favorites, dispatch] = useReducer(favoritesReducer, []);
+  const [message, setMessage] = useState('');
 
   const addToCart = (item) => {
     setCart([...cart, item]);
-    console.log(`${item.title} added to cart.`);
+    setMessage(`Added ${item.title} to the cart.`);
   };
 
-  const removeFromCart = (itemRemove) => {
-    setCart(cart.filter(item => item !== itemRemove));
-    console.log(`Item removed from cart.`);
+  const removeFromCart = (item) => {
+    setCart(cart.filter(cartItem => cartItem.id !== item.id));
+    setMessage(`Removed ${item.title} from cart.`);
   };
-  //variable coordination...not sure if favorites placement is correct//
-  const addToFavorite = (itemFavorite, favorites) => {
-    dispatch({ type: 'ADD_TO_FAVORITE', payload: itemFavorite });
-    console.log(`Item added to favorite.`);
+
+  const addToFavorite = (item) => {
+    const updatedItems = mediaItems.map(mediaItem =>
+      mediaItem.id === item.id ? { ...mediaItem, isFavorite: !mediaItem.isFavorite } : mediaItem
+    );
+    setMediaItems(updatedItems);
   };
 
   return (
     <Router>
-      <body>
       <div className="App">
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/vhs'>VHS</Link>
+          <Link to='/dvd'>DVD</Link>
+          <Link to='/cd'>CD</Link>
+        </nav>
+        {message && <div>{message}</div>}
         <h1>Retro Media Store</h1>
-          <Route path="/" exact component={Home} />
-          <Route path="/media/:type/:id">
-            <MediaDetail mediaItems={mediaItems} addToCart={addToCart} addToFavorite={addToFavorite} />
-          </Route>
-        <MediaList mediaItems={mediaItems} addToCart={addToCart} removeFromCart={removeFromCart} />
-        <Message />
+        <Routes>
+          <Route path='/' element={<MediaList mediaItems={mediaItems} addToCart={addToCart} removeFromCart={removeFromCart} addToFavorite={addToFavorite} />} exact />
+          <Route path='/vhs' element={<MediaList mediaItems={mediaItems.filter(item => item.type === 'VHS')} addToCart={addToCart} removeFromCart={removeFromCart} addToFavorite={addToFavorite} />} />
+          <Route path='/dvd' element={<MediaList mediaItems={mediaItems.filter(item => item.type === 'DVD')} addToCart={addToCart} removeFromCart={removeFromCart} addToFavorite={addToFavorite} />} />
+          <Route path='/cd' element={<MediaList mediaItems={mediaItems.filter(item => item.type === 'CD')} addToCart={addToCart} removeFromCart={removeFromCart} addToFavorite={addToFavorite} />} />
+        </Routes>
       </div>
-      </body>
     </Router>
   );
 }
